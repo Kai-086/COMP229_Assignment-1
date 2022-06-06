1e8c71e9fc29cd5fea57deb46b48097290da5fea
Kai <bono6415@gmail.com>
2022-06-05 23:22:24 -0400

    added all content

---
+++ .history/Client/Scripts/app_20220605232057.js
@@ -0,0 +1,37 @@
+"use strict";
+(function () {
+    function Start() {
+        console.log("App Started!");
+        let XHR = new XMLHttpRequest();
+        XHR.open("GET", "../data/data.json");
+        XHR.send();
+        XHR.addEventListener("readystatechange", function () {
+            if (XHR.readyState == 4 && XHR.status == 200) {
+                console.log("JSON Data:");
+                console.log("===========");
+                console.log(XHR.responseText);
+            }
+        });
+    }
+    window.addEventListener("load", Start);
+
+    function Sent() {
+        let emailBtn = document.getElementById('email_btn');
+
+        emailBtn.addEventListener("click", function () {
+            // get value
+            let name = document.getElementById('name').value;
+            let email = document.getElementById('email').value;
+            let subject = document.getElementById('subject').value;
+
+            let content = "" + document.getElementById('content').value + '%0A%0A';
+            content += "From:" + name.value + '%0a';
+            content += "Email:" + email.value + '%0a';
+
+            mailTo.href = "mailto:" + to + "?subject" + "&body=" + body;
+            mailTo.click();
+        });
+    }
+    window.addEventListener("load", Sent)
+})();
+//# sourceMappingURL=app.js.map
